4ece377f2c47519cf4cc4687f6fa34e1c281bd41
cany <couleurvide1008@gmail.com>
2023-11-10 14:14:15 +0900

    sp-menuがactiveなときはスクロールしないよう記述を追加

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-// sp-menu開閉
+// sp-menu開閉,スクロール制御
 const headerButton = document.getElementById("header__button");
 const menuButton = document.getElementById("sp-menu__button__close");
 const spMenu = document.getElementById("sp-menu");
 
+const noscroll = (e) => {
+  e.preventDefault();
+};
+
 headerButton.addEventListener("click", function () {
   spMenu.classList.toggle("active");
   spMenu.classList.remove("inactive");
+  document.addEventListener("touchmove", noscroll, { passive: false });
+  document.addEventListener("wheel", noscroll, { passive: false });
 });
 
 menuButton.addEventListener("click", function () {
   spMenu.classList.toggle("active");
   spMenu.classList.add("inactive");
+  document.removeEventListener("touchmove", noscroll);
+  document.removeEventListener("wheel", noscroll);
 });
 
 // swiper
